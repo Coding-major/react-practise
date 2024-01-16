e0e4b7003964f19c44b0a920ca2e99b409287307
coding-major <shehumustapham@gmail.com>
2024-01-16 10:20:26 +0100

    use state beginning with simple number increament and decreament

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header"
 import notes from "../list"
 import Note from "./notes";
 
 
+
 function App() {
+
+    const [begin, setBegin] = useState(0);
+
+    function increase() {
+        setBegin(begin + 1)
+    }
+
+    function decrease() {
+        setBegin(begin - 1)
+    }
+
     return (
         <div>
             <Header />
             <div className="main">
-                {notes.map((note) => {
-                    return <Note title={note.title} content={note.content} />
-                })}
+                <h1>{begin}</h1>
+
+                    <button onClick={increase}>+</button>
+                    <button onClick={decrease}>-</button>
+                    
             </div>
             <footer />
         </div>
