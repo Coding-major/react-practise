0bb762fcb0df49cf907c03d70f02615122b6fd21
coding-major <shehumustapham@gmail.com>
2024-01-18 13:40:52 +0100

    strikeThrough an item if clicked, and also un-strike-thorugh if clicked again

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 function Todo(props) {
-    // var [srikeThrough, setStrikeThrough] = useState(false)
+    var [srikeThrough, setStrikeThrough] = useState(false)
 
-    // function changeStrike () {
-    //     setStrikeThrough()
-    // }
-    return <li>{props.name}</li>
+    function changeStrike () {
+        setStrikeThrough((previousValue) => {
+            if(previousValue === false) {
+                return true
+            } else if (previousValue === true) {
+                return false
+            }
+        })
+    }
+    return <li onClick={changeStrike} style={{textDecoration: srikeThrough ? "line-through" : null}}>{props.name}</li>
 }
 
 export default Todo;
